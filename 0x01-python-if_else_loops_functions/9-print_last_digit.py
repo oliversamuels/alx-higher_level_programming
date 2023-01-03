d2301dce2377bd927a730c1e52d913e7b6706531
Samuel Amos <amoskevinsamuel@hotmail.com>
2023-01-03 07:50:02 -0800

    Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_value = abs(number) % 10
-    if number < 0:
-        last_value = -last_value
-        return last_value
-    return last_value
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
